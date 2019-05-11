b26798ac953f677fea131c2466a48b5bef397ec1
Unknown <hdudowicz@gmail.com>
2019-05-12 00:07:58 +0100

    Work

--- TicTacToe/TicTacToe/Program.fs
+++ TicTacToe/TicTacToe/Program.fs
@@ -4,43 +4,140 @@ open System
 open Elmish
 open Elmish.WPF
 
-type Model =
-    {   Count: int
-        StepSize: int 
-        Shape: string}
+//game design
+module TicTacToeDesign =
+    //players X and O
+    type Player = PlayerX | PlayerO
 
-let init () =
-    {   Count = 0
-        StepSize = 1 
-        Shape = "circle.jpg"}
+    //board positions
+    //board made of horizontal and vertical components
+    type HorizontalPosition = Left | Center | Right 
+    type VerticalPosition = Top | Center | Bottom
+    type SpacePosition = HorizontalPosition * VerticalPosition
+
+    //board spaces
+    //Spaces either empty or used 
+    //(used when an X or O is placed on them)
+    //space made of its position and state
+    type SpaceSate = Taken of Player | Empty
+
+    type Space = {
+        position : SpacePosition 
+        state : SpaceSate
+    }
+
+
+    //player position
+    //postion player puts a counter on their turn
+    type PlayerXPosition = PlayerXPosition of SpacePosition
+    type PlayerOPosition = PlayerOPosition of SpacePosition
+
+    //vaild places player can put a counter on their turn
+    //places to put a counter decided from making a list of the positions taken
+    type PlayerXValidPlaces = PlayerXPosition list
+    type PlayerOValidPlaces = PlayerOPosition list
+
+    //turn 
+    type TurnResult = 
+        | PlayerXTurn of PlayerXValidPlaces
+        | PlayerOTurn of PlayerOValidPlaces
+        | WinGame of Player
+    //When a player places their counter, this position is then taken.
+    //GameState is updated to save all the taken positions by 
+    //adding this position to the other taken 1s from previous turns
+    //input -> output
+    type PlayerXPlacesCounter<'GameState> = 'GameState * PlayerXPosition -> 'GameState * TurnResult
+    type PlayerOPlacesCounter<'GameState> = 'GameState * PlayerOPosition -> 'GameState * TurnResult
+    //New game made by resetting TurnResult and GameState
+    type NewGame<'GameState> = 'GameState * TurnResult
+
+//game logic 
+module TicTacToeLogic =
+
+    open TicTacToeDesign
+
+    type Model =
+        {   Count: int
+            CurrentPlayer: Player 
+            Shape: string}
+    
+    let init () =
+        {   Count = 0
+            //StepSize = 1 
+            CurrentPlayer = PlayerX
+            Shape = "cross.jpg"}
+
+    let doMove p m : Model = m.CurrentPlayer = PlayerO 
+
+    //GameState used to keep track of the game
+    //saves which board spaces are taken 
+    type GameState = {
+        spaces : Space list
+    }
+
+    //TODO: List horizontal positions 
+
+
+    //TODO: List vertial positions 
+
+
+    //TODO: Line of SpacePostions list
+
+
+    //TODO: Check for 3 in a row
+        //3 in a row vertial lines 
+        //3 in a row horizontal lines 
+        //diagonal 
+        //other diagonal 
+
+    //TODO: Check lines for win
+
+
+
+//game user interface
+//module TicTacToeInterface =
+
+open TicTacToeDesign
+
+
+
+//if CurrentPlayer = PlayerX then "cross.jpg" else "circle.jpg"
+
+
+open TicTacToeLogic
 
 type Msg =
-    | ChangeShape
-    | Increment
-    | Decrement
-    | SetStepSize of int
-    | Reset
+    | ChangeShape of string
+    //| Increment
+    //| SetStepSize of int
+    | Reset 
 
 let update msg m =
     match msg with
-        | ChangeShape -> {m with Shape = "cross.png"}
-        | Increment -> { m with Count = m.Count + m.StepSize }
-        | Decrement -> { m with Count = m.Count - m.StepSize }
-        | SetStepSize x -> { m with StepSize = x }
+        //| ChangeShape -> {m with Shape = if m.CurrentPlayer = PlayerX then "cross.jpg" else "circle.jpg"}
+
+        //ChangeShape message passes coordinates of pressed button as parameter p, turn is handled in doMove function
+        //doMove takes then position and current model as paramenters
+        | ChangeShape p -> doMove p m
+        //| Increment -> { m with Count = m.Count + m.StepSize }
+        //| SetStepSize x -> { m with StepSize = x }
         | Reset -> init ()
 
 open Elmish.WPF
 
 let bindings model dispatch =
     [
-        "ChangeShape" |> Binding.cmd (fun m -> ChangeShape)
+        //"ChangeShape" |> Binding.cmd (fun m -> ChangeShape)
+        
+        //ChangeShape calls function with coordinate parameter declared in the XAML
+        //Pipes position parameter from XAML into ChangeShape then doMove 
+        "ChangeShape" |> Binding.paramCmd (fun p m -> string p |> ChangeShape)
         "Shape" |> Binding.oneWay (fun m -> m.Shape)
         "CounterValue" |> Binding.oneWay (fun m -> m.Count)
-        "Increment" |> Binding.cmd (fun m -> Increment)
-        "Decrement" |> Binding.cmd (fun m -> Decrement)
-        "StepSize" |> Binding.twoWay
-          (fun m -> float m.StepSize)
-          (fun newVal m -> int newVal |> SetStepSize)
+        //"Increment" |> Binding.cmd (fun m -> Increment)
+        //"StepSize" |> Binding.twoWay
+        //  (fun m -> float m.StepSize)
+        //  (fun newVal m -> int newVal |> SetStepSize)
     ]
 
 
